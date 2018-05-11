4d8c512b0d07802b51b6108f88ae43f18f3baebb
administrator@LIBERTYJAYAONE.LOCAL <administrator@LIBERTYJAYAONE.LOCAL>
2018-05-11 13:56:47 +0700

    --

--- install.sql
+++ install.sql
@@ -1,13 +1,13 @@
-drop FUNCTION [gen].generateKeys
-drop FUNCTION [gen].retrievePublicKey
-drop FUNCTION [gen].getSignature
-drop FUNCTION [gen].verifySignature
-drop FUNCTION [gen].getSharedSecret
-drop FUNCTION [gen].getDerivedKey
-drop FUNCTION [gen].encrypt2F
-drop FUNCTION [gen].decrypt2F
-drop FUNCTION [gen].encryptAES
-drop FUNCTION [gen].decryptAES
+if exists(select * from sys.objects where name='generateKeys') drop FUNCTION [gen].generateKeys
+if exists(select * from sys.objects where name='retrievePublicKey') drop FUNCTION [gen].retrievePublicKey
+if exists(select * from sys.objects where name='getSignature') drop FUNCTION [gen].getSignature
+if exists(select * from sys.objects where name='verifySignature') drop FUNCTION [gen].verifySignature
+if exists(select * from sys.objects where name='getSharedSecret') drop FUNCTION [gen].getSharedSecret
+if exists(select * from sys.objects where name='getDerivedKey') drop FUNCTION [gen].getDerivedKey
+if exists(select * from sys.objects where name='encrypt2F') drop FUNCTION [gen].encrypt2F
+if exists(select * from sys.objects where name='decrypt2F') drop FUNCTION [gen].decrypt2F
+if exists(select * from sys.objects where name='encryptAES') drop FUNCTION [gen].encryptAES
+if exists(select * from sys.objects where name='decryptAES') drop FUNCTION [gen].decryptAES
 go
 
 create FUNCTION [gen].[generateKeys](@key nvarchar(4), @network nvarchar(10))
@@ -43,22 +43,22 @@ RETURNS nvarchar(100) WITH EXECUTE AS CALLER
 AS 
 EXTERNAL NAME cryptoSQL.[cryptoSQL.functions].getDerivedKey
 go
-create FUNCTION [gen].encrypt2F(@m nvarchar(max), @p nvarchar(100), @s nvarchar(100))
-RETURNS nvarchar(100) WITH EXECUTE AS CALLER
+create FUNCTION [gen].encrypt2F(@m nvarchar(max), @p nvarchar(max), @s nvarchar(max))
+RETURNS nvarchar(max) WITH EXECUTE AS CALLER
 AS 
 EXTERNAL NAME cryptoSQL.[cryptoSQL.functions].encrypt2F
 go
-create FUNCTION [gen].decrypt2F(@m nvarchar(max), @p nvarchar(100), @s nvarchar(100))
-RETURNS nvarchar(100) WITH EXECUTE AS CALLER
+create FUNCTION [gen].decrypt2F(@m nvarchar(max), @p nvarchar(max), @s nvarchar(max))
+RETURNS nvarchar(max) WITH EXECUTE AS CALLER
 AS 
 EXTERNAL NAME cryptoSQL.[cryptoSQL.functions].decrypt2F
 go
-create FUNCTION [gen].encryptAES(@m nvarchar(max), @k nvarchar(100), @s nvarchar(100))
-RETURNS nvarchar(100) WITH EXECUTE AS CALLER
+create FUNCTION [gen].encryptAES(@m nvarchar(max), @k nvarchar(max), @s nvarchar(max))
+RETURNS nvarchar(max) WITH EXECUTE AS CALLER
 AS 
 EXTERNAL NAME cryptoSQL.[cryptoSQL.functions].encryptAES
 go
-create FUNCTION [gen].decryptAES(@m nvarchar(max), @k nvarchar(100), @s nvarchar(100))
+create FUNCTION [gen].decryptAES(@m nvarchar(max), @k nvarchar(max), @s nvarchar(max))
 RETURNS nvarchar(max) WITH EXECUTE AS CALLER
 AS 
 EXTERNAL NAME cryptoSQL.[cryptoSQL.functions].decryptAES
