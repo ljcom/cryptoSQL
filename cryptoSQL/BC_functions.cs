bdd98f7d19796279f9f241fba65d2ea316cc7653
Samuel <samuel@libertyjayaone.local>
2018-05-11 11:58:10 +0700

    fist commit

---
+++ cryptoSQL/BC_functions.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Security;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Generators;
+using Org.BouncyCastle.Asn1.Nist;
+using Org.BouncyCastle.Asn1.X9;
+using Org.BouncyCastle.Math.EC;
+using Org.BouncyCastle.Math;
+using Org.BouncyCastle.Crypto.Macs;
+using Org.BouncyCastle.Crypto.Digests;
+using Org.BouncyCastle.Crypto.Agreement;
+using Org.BouncyCastle.Crypto.Agreement.Kdf;
+using Org.BouncyCastle.Crypto.Engines;
+using Org.BouncyCastle.Crypto.Paddings;
+using Org.BouncyCastle.Crypto.Modes;
+using System.Text;
+
+namespace cryptoSQL
+{
+    static class BCFunctions
+    {
+        public static AsymmetricCipherKeyPair GenerateKeys(int keySize)
+        {
+            var gen = new ECKeyPairGenerator();
+            var secureRandom = new SecureRandom();
+            var keyGenParam = new KeyGenerationParameters(secureRandom, keySize);
+            gen.Init(keyGenParam);
+            return gen.GenerateKeyPair();
+
+        }
+        public static string RetrievePublicKey(string keySize, BigInteger d)
+        {
+            X9ECParameters ecParams = NistNamedCurves.GetByName("P-" + keySize);
+            ECDomainParameters domainParameters = new ECDomainParameters(ecParams.Curve, ecParams.G, ecParams.N, ecParams.H, ecParams.GetSeed());
+
+            //public key
+            ECPoint q = domainParameters.G.Multiply(d);
+            ECPublicKeyParameters pubkeyParam = new ECPublicKeyParameters(q, domainParameters);
+
+            return Convert.ToBase64String(pubkeyParam.Q.GetEncoded());
+
+        }
+
+        public static bool VerifySignature(AsymmetricCipherKeyPair key, string plainText, byte[] signature)
+        {
+            var encoder = new ASCIIEncoding();
+            var inputData = encoder.GetBytes(plainText);
+            var signer = SignerUtilities.GetSigner("ECDSA");
+            signer.Init(false, key.Public);
+            signer.BlockUpdate(inputData, 0, inputData.Length);
+            return signer.VerifySignature(signature);
+        }
+
+
+        public static byte[] GetSignature(string plainText, ICipherParameters privateKey)
+        //private static byte[] GetSignature(string plainText, AsymmetricCipherKeyPair key)
+        {
+            /* Init alg */
+            var signer = SignerUtilities.GetSigner("ECDSA");
+
+            /* Populate key */
+            //signer.Init(true, key.Private);
+            signer.Init(true, privateKey);
+
+            /* Get the bytes to be signed from the string */
+            var bytes = Encoding.UTF8.GetBytes(plainText);
+
+            /* Calc the signature */
+            signer.BlockUpdate(bytes, 0, bytes.Length);
+            byte[] signature = signer.GenerateSignature();
+
+            /* Base 64 encode the sig so its 8-bit clean */
+            //var signedString = Convert.ToBase64String(signature);
+            //return signedString;
+            return signature;
+        }
+
+        public static byte[] Hash(string text, string key)
+        {
+            var hmac = new HMac(new Sha256Digest());
+            hmac.Init(new KeyParameter(Encoding.UTF8.GetBytes(key)));
+            byte[] result = new byte[hmac.GetMacSize()];
+            byte[] bytes = Encoding.UTF8.GetBytes(text);
+
+            hmac.BlockUpdate(bytes, 0, bytes.Length);
+            hmac.DoFinal(result, 0);
+
+            return result;
+        }
+
+        public static Byte[] getSharedSecret(Byte[] PrivateKeyIn, Byte[] PublicKeyIn)
+        {
+            ECDHCBasicAgreement agreement = new ECDHCBasicAgreement();
+            X9ECParameters curve = null;
+            ECDomainParameters ecParam = null;
+            ECPrivateKeyParameters privKey = null;
+            ECPublicKeyParameters pubKey = null;
+            ECPoint point = null;
+
+            curve = NistNamedCurves.GetByName("P-256");
+            ecParam = new ECDomainParameters(curve.Curve, curve.G, curve.N, curve.H, curve.GetSeed());
+            privKey = new ECPrivateKeyParameters(new BigInteger(PrivateKeyIn), ecParam);
+            point = ecParam.Curve.DecodePoint(PublicKeyIn);
+            pubKey = new ECPublicKeyParameters(point, ecParam);
+
+            agreement.Init(privKey);
+
+            BigInteger secret = agreement.CalculateAgreement(pubKey);
+
+            return secret.ToByteArrayUnsigned();
+        }
+
+        public static byte[] DeriveSymmetricKeyFromSharedSecret(byte[] sharedSecret)
+        {
+            ECDHKekGenerator egH = new ECDHKekGenerator(DigestUtilities.GetDigest("SHA256"));
+
+            egH.Init(new DHKdfParameters(NistObjectIdentifiers.Aes, sharedSecret.Length, sharedSecret));
+            byte[] symmetricKey = new byte[DigestUtilities.GetDigest("SHA256").GetDigestSize()];
+            egH.GenerateBytes(symmetricKey, 0, symmetricKey.Length);
+
+            return symmetricKey;
+        }
+
+    }
+    static class BCDES
+    {
+        public static byte[] EncryptDES(byte[] data, byte[] derivedKey)
+        {
+            byte[] output = null;
+            try
+            {
+                KeyParameter keyparam = ParameterUtilities.CreateKeyParameter("DES", derivedKey);
+                IBufferedCipher cipher = CipherUtilities.GetCipher("DES/ECB/ISO7816_4PADDING");
+                cipher.Init(true, keyparam);
+                try
+                {
+                    output = cipher.DoFinal(data);
+                    return output;
+                }
+                catch (System.Exception ex)
+                {
+                    throw new CryptoException(ex.Message);
+                }
+            }
+            catch
+            {
+
+            }
+
+            return output;
+        }
+        public static byte[] DecryptDES(byte[] cipherData, byte[] derivedKey)
+        {
+            byte[] output = null;
+            try
+            {
+                KeyParameter keyparam = ParameterUtilities.CreateKeyParameter("DES", derivedKey);
+                IBufferedCipher cipher = CipherUtilities.GetCipher("DES/ECB/ISO7816_4PADDING");
+                cipher.Init(false, keyparam);
+                try
+                {
+                    output = cipher.DoFinal(cipherData);
+
+                }
+                catch (System.Exception ex)
+                {
+                    throw new CryptoException(ex.Message);
+                }
+            }
+            catch 
+            {
+            }
+
+            return output;
+        }
+    }
+    static class BCAES
+    {
+        public static byte[] EncryptAES(byte[] inputBytes, byte[] key, byte[] iVector)
+        {
+            //Convert.FromBase64String(keyString);
+            //Set up
+            byte[] iv = iVector; //new byte[16];
+
+            AesEngine engine = new AesEngine();
+            CbcBlockCipher blockCipher = new CbcBlockCipher(engine); //CBC
+            PaddedBufferedBlockCipher cipher = new PaddedBufferedBlockCipher(blockCipher); //Default scheme is PKCS5/PKCS7
+            KeyParameter keyParam = new KeyParameter(key);
+            ParametersWithIV keyParamWithIV = new ParametersWithIV(keyParam, iv, 0, 16);
+
+            // Encrypt
+            cipher.Init(true, keyParamWithIV);
+            byte[] outputBytes = new byte[cipher.GetOutputSize(inputBytes.Length)];
+            int length = cipher.ProcessBytes(inputBytes, outputBytes, 0);
+            cipher.DoFinal(outputBytes, length); //Do the final block
+                                                 //string encryptedInput = Convert.ToBase64String(outputBytes);
+
+            //cipher.Init(false, keyParamWithIV);
+            //byte[] comparisonBytes = new byte[cipher.GetOutputSize(outputBytes.Length)];
+            //length = cipher.ProcessBytes(outputBytes, comparisonBytes, 0);
+            //cipher.DoFinal(comparisonBytes, length); //Do the final block
+
+            return outputBytes;
+        }
+        public static byte[] DecryptAES(byte[] outputBytes, byte[] key, byte[] iVector)
+        {
+            byte[] iv = iVector; //new byte[16]; 
+
+            AesEngine engine = new AesEngine();
+            CbcBlockCipher blockCipher = new CbcBlockCipher(engine); //CBC
+            PaddedBufferedBlockCipher cipher = new PaddedBufferedBlockCipher(blockCipher); //Default scheme is PKCS5/PKCS7
+            KeyParameter keyParam = new KeyParameter(key);
+            ParametersWithIV keyParamWithIV = new ParametersWithIV(keyParam, iv, 0, 16);
+
+            //Decrypt            
+            cipher.Init(false, keyParamWithIV);
+            byte[] comparisonBytes = new byte[cipher.GetOutputSize(outputBytes.Length)];
+            int length = cipher.ProcessBytes(outputBytes, comparisonBytes, 0);
+            cipher.DoFinal(comparisonBytes, length); //Do the final block
+
+            return comparisonBytes;
+        }
+    }
+    public class BC2F
+    {
+        private readonly Encoding _encoding;
+        private readonly IBlockCipher _blockCipher;
+        private PaddedBufferedBlockCipher _cipher;
+        private IBlockCipherPadding _padding;
+
+        public BC2F()
+        {
+
+        }
+
+        BC2F(IBlockCipher blockCipher, Encoding encoding)
+        {
+            _blockCipher = blockCipher;
+            _encoding = encoding;
+        }
+
+        #region Public Methods
+
+
+        public static string Encrypt2F(string TextPlain, string Password, byte[] Salt)
+        {
+            Sha3Digest Sha3Digest = new Sha3Digest();
+            Pkcs5S2ParametersGenerator gen = new Pkcs5S2ParametersGenerator(Sha3Digest);
+            gen.Init(Encoding.UTF8.GetBytes(Password), Salt, 1000);
+            KeyParameter param = (KeyParameter)gen.GenerateDerivedParameters(new TwofishEngine().AlgorithmName, 256);
+
+            BC2F bcEngine = new BC2F(new TwofishEngine(), Encoding.UTF8);
+            bcEngine.SetPadding(new Pkcs7Padding());
+            return bcEngine.Encrypt(TextPlain, param);
+        }
+
+        public static string Decrypt2F(string TextEncripted, string Password, byte[] Salt)
+        {
+            Sha3Digest Sha3Digest = new Sha3Digest();
+            Pkcs5S2ParametersGenerator gen = new Pkcs5S2ParametersGenerator(Sha3Digest);
+            gen.Init(Encoding.UTF8.GetBytes(Password), Salt, 1000);
+            KeyParameter param = (KeyParameter)gen.GenerateDerivedParameters(new TwofishEngine().AlgorithmName, 256);
+
+            BC2F bcEngine = new BC2F(new TwofishEngine(), Encoding.UTF8);
+            bcEngine.SetPadding(new Pkcs7Padding());
+            return bcEngine.Decrypt(TextEncripted, param);
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        void SetPadding(IBlockCipherPadding padding)
+        {
+            if (padding != null)
+                _padding = padding;
+        }
+
+        string Encrypt(string plain, ICipherParameters SetKeyParameter)
+        {
+            byte[] result = BouncyCastleCrypto(true, _encoding.GetBytes(plain), SetKeyParameter);
+            return Convert.ToBase64String(result);
+        }
+
+        string Decrypt(string cipher, ICipherParameters SetKeyParameter)
+        {
+            byte[] result = BouncyCastleCrypto(false, Convert.FromBase64String(cipher), SetKeyParameter);
+            return _encoding.GetString(result, 0, result.Length);
+        }
+
+        byte[] BouncyCastleCrypto(bool forEncrypt, byte[] input, ICipherParameters SetKeyParameter)
+        {
+            try
+            {
+                _cipher = _padding == null ?
+                new PaddedBufferedBlockCipher(_blockCipher) : new PaddedBufferedBlockCipher(_blockCipher, _padding);
+
+                _cipher.Init(forEncrypt, SetKeyParameter);
+
+                byte[] ret = _cipher.DoFinal(input);
+                return ret;
+
+            }
+            catch (CryptoException ex)
+            {
+                Console.Write(ex.Message);
+                //MessageBox(ex.Message);
+            }
+            return null;
+        }
+
+        #endregion
+
+    }
+
+}
+
+
---
+++ cryptoSQL/cryptoSQL_functions.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using Microsoft.SqlServer.Server;
+using System.Data.SqlTypes;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Math;
+using Org.BouncyCastle.Asn1.X9;
+using Org.BouncyCastle.Asn1.Nist;
+using Org.BouncyCastle.Math.EC;
+using Org.BouncyCastle.Security;
+using System.Xml;
+
+namespace cryptoSQL
+{
+    public static class functions
+    {
+        [Microsoft.SqlServer.Server.SqlFunction(DataAccess = DataAccessKind.Read)]
+        public static SqlXml generateKeys(SqlString keySize, SqlString network)
+        {
+            AsymmetricCipherKeyPair key = BCFunctions.GenerateKeys(Convert.ToInt16(keySize.ToString()));
+
+            ECPublicKeyParameters publicKeyParam = (ECPublicKeyParameters)key.Public;
+            ECPrivateKeyParameters privateKeyParam = (ECPrivateKeyParameters)key.Private;
+
+            var privateKey = privateKeyParam.D.ToString();
+            BigInteger d = new BigInteger(privateKey, 10);
+
+            var publicKey = BCFunctions.RetrievePublicKey(keySize.ToString(), d);
+
+            var addressText = "0x" + BitConverter.ToString(BCFunctions.Hash(publicKey, network.ToString())).Replace("-", "");
+
+            var r = "<sqroot><D>" + privateKey + "</D><Q>" + publicKey + "</Q><A>" + addressText + "</A></sqroot>";
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(r);
+            XmlNodeReader xnr = new XmlNodeReader(xmlDoc);
+            SqlXml rx = new SqlXml(xnr);
+            return (rx);
+        }
+
+        [Microsoft.SqlServer.Server.SqlFunction(DataAccess = DataAccessKind.Read)]
+        public static SqlString retrievePublicKey(SqlString keySize, SqlString privateKey)
+        {
+            BigInteger DString = new BigInteger(privateKey.ToString(), 10);
+            return BCFunctions.RetrievePublicKey(keySize.ToString(), DString);
+        }
+
+        [Microsoft.SqlServer.Server.SqlFunction(DataAccess = DataAccessKind.Read)]
+        public static SqlString getSignature(SqlString keySize, SqlString PrivateKey, SqlString Message)
+        {
+            X9ECParameters ecParams = NistNamedCurves.GetByName("P-" + keySize.ToString());
+            ECDomainParameters domainParameters = new ECDomainParameters(ecParams.Curve, ecParams.G, ecParams.N, ecParams.H, ecParams.GetSeed());
+            var G = ecParams.G;
+            ECCurve curve = ecParams.Curve;
+
+            //private Key
+            BigInteger d = new BigInteger(PrivateKey.ToString(), 10);
+            ECPrivateKeyParameters keyParameters = new ECPrivateKeyParameters(d, domainParameters);
+
+            //public key
+            ECPoint q = domainParameters.G.Multiply(d);
+            ECPublicKeyParameters pubkeyParam = new ECPublicKeyParameters(q, domainParameters);
+
+            string s = Message.ToString();
+            var signature = BCFunctions.GetSignature(s, keyParameters);
+            var signedString = Convert.ToBase64String(signature);
+            var txtSignature = signedString;
+            return txtSignature;
+        }
+
+        [Microsoft.SqlServer.Server.SqlFunction(DataAccess = DataAccessKind.Read)]
+        public static SqlBoolean verifySignature(SqlString keySize, SqlString PublicKey, SqlString message, SqlString signature)
+        {
+
+            byte[] messageBytes = Encoding.ASCII.GetBytes(message.ToString());
+            byte[] signatureBytes = Convert.FromBase64String(signature.ToString());
+
+            X9ECParameters ecParams = NistNamedCurves.GetByName("P-" + keySize.ToString());
+            ECDomainParameters domainParameters = new ECDomainParameters(ecParams.Curve, ecParams.G, ecParams.N, ecParams.H, ecParams.GetSeed());
+            var G = ecParams.G;
+            Org.BouncyCastle.Math.EC.ECCurve curve = ecParams.Curve;
+
+            byte[] encoded = Convert.FromBase64String(PublicKey.ToString());
+            ECPoint q = curve.DecodePoint(encoded);
+
+            try
+            {
+                ECPublicKeyParameters pubkeyParam = new ECPublicKeyParameters(q, domainParameters);
+                var verifier = SignerUtilities.GetSigner("ECDSA");
+                verifier.Init(false, pubkeyParam);
+                verifier.BlockUpdate(messageBytes, 0, messageBytes.Length);
+                bool signatureOK = verifier.VerifySignature(signatureBytes);
+
+                return signatureOK;
+
+            }
+            catch
+            {
+                return (false);
+            }
+        }
+
+        [Microsoft.SqlServer.Server.SqlFunction(DataAccess = DataAccessKind.Read)]
+        public static SqlString getSharedSecret(SqlString keySize, SqlString PrivateKey, SqlString PublicKey)
+        {
+            //X9ECParameters ecP = NistNamedCurves.GetByName("P-" + keySize.ToString());
+            //var c = (FpCurve)ecP.Curve;
+            //var eCDomainParameters = new ECDomainParameters(ecP.Curve, ecP.G, ecP.N, ecP.H, ecP.GetSeed());
+
+            BigInteger dA = new BigInteger(PrivateKey.ToString(), 10);
+            //ECPrivateKeyParameters keyParametersA = new ECPrivateKeyParameters(dA, eCDomainParameters);
+            byte[] pkBytes = dA.ToByteArray();
+
+            byte[] pbBytes = Convert.FromBase64String(PublicKey.ToString());
+
+            byte[] ss = BCFunctions.getSharedSecret(pkBytes, pbBytes);
+            return Convert.ToBase64String(ss);
+
+        }
+
+        [Microsoft.SqlServer.Server.SqlFunction(DataAccess = DataAccessKind.Read)]
+        public static SqlString getDerivedKey(SqlString sharedSecret)
+        {
+            byte[] ssBytes = Encoding.ASCII.GetBytes(sharedSecret.ToString());
+            try
+            {
+                byte[] dk = BCFunctions.DeriveSymmetricKeyFromSharedSecret(ssBytes);
+                return Convert.ToBase64String(dk);
+            }
+            catch { return ""; }
+        }
+
+        [Microsoft.SqlServer.Server.SqlFunction(DataAccess = DataAccessKind.Read)]
+        public static SqlString encrypt2F(SqlString message, SqlString pwd, SqlString salt)
+        {
+            try
+            {
+                byte[] saltBytes = Encoding.ASCII.GetBytes(salt.ToString());
+                return BC2F.Encrypt2F(message.ToString(), pwd.ToString(), saltBytes);
+            }
+            catch (Exception ex) { return ex.Message; }
+
+        }
+
+        [Microsoft.SqlServer.Server.SqlFunction(DataAccess = DataAccessKind.Read)]
+        public static SqlString decrypt2F(SqlString message, SqlString pwd, SqlString salt)
+        {
+            byte[] saltBytes = Encoding.ASCII.GetBytes(salt.ToString());
+            try
+            {
+                return BC2F.Decrypt2F(message.ToString(), pwd.ToString(), saltBytes);
+            }
+            catch (Exception ex) { return ex.Message; }
+        }
+
+        [Microsoft.SqlServer.Server.SqlFunction(DataAccess = DataAccessKind.Read)]
+        public static SqlString encryptAES(SqlString message, SqlString key, SqlString iv)
+        {
+
+            //byte[] key = Convert.FromBase64String(txtDKA.Text);
+
+            //byte[] iv = new byte[16]; //derivedKeyA.Take(16).ToArray();
+
+            byte[] mBytes = Encoding.UTF8.GetBytes(message.ToString());
+            byte[] keyBytes = Convert.FromBase64String(key.ToString());
+            byte[] ivBytes = Convert.FromBase64String(iv.ToString());
+            try
+            {
+                return Convert.ToBase64String(BCAES.EncryptAES(mBytes, keyBytes, ivBytes));
+            }
+            catch (Exception ex) { return ex.Message; }
+        }
+
+        [Microsoft.SqlServer.Server.SqlFunction(DataAccess = DataAccessKind.Read)]
+        public static SqlString decryptAES(SqlString message, SqlString key, SqlString iv)
+        {
+            byte[] mBytes = Convert.FromBase64String(message.ToString());
+            byte[] keyBytes = Convert.FromBase64String(key.ToString());
+            byte[] ivBytes = Convert.FromBase64String(iv.ToString());
+            try
+            {
+                byte[] dText = BCAES.DecryptAES(mBytes, keyBytes, ivBytes);
+                return Encoding.UTF8.GetString(dText);
+            }
+            catch (Exception ex) { return ex.Message; }   
+        }
+    }
+
+}
